fb03872854bd092c46e1d5032d2bfe487ef1cd95
Ubuntu <ubuntu@ip-10-112-221-25.ec2.internal>
2013-07-11 05:37:47 +0000

    fs and buffer in web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var buf = fs.readFileSync("index.html");
+    var b = buf.toString();
+    response.send(b);
 });
 
 var port = process.env.PORT || 5000;
